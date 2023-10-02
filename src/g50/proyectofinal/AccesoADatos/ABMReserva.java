04f820ac5010df263e07e8e84467be2f8fd2b42a
Nicolas <Nicolasgabrielsanchez7@gmail.com>
2023-10-02 11:40:05 -0300

    asd

--- src/g50/proyectofinal/AccesoADatos/ABMReserva.java
+++ src/g50/proyectofinal/AccesoADatos/ABMReserva.java
@@ -9,5 +9,16 @@ package g50.proyectofinal.AccesoADatos;
  * @author whatu
  */
 public class ABMReserva {
+//    ABM Reserva. En cada Reserva se toma nota de: datos del Huesped, Tipo de Habitación, cantidad de personas, fechas de entrada y salida, importe total de estadía. Metodos Buscar una Reserva por Huesped, Cancelar reserva
+//
+//
+//El proceso de hacer una Reserva se organizará. El método Crear Reserva:
+//Se introduce por teclado fechaEntrada y fechaSalida y cantidad de personas.
+//Se busca un “tipo de habitación” para esa cantidad de personas y el precio. 
+//Se devuelve una lista de las habitaciones que estén Libres, de ese “Tipo Habitación”.
+//
+//    
+//    
+    
     
 }
--- src/g50/proyectofinal/Entidades/ABMHabitaciones.java
+++ src/g50/proyectofinal/Entidades/ABMHabitaciones.java
@@ -9,5 +9,8 @@ package g50.proyectofinal.Entidades;
  * @author whatu
  */
 public class ABMHabitaciones {
+//    ABM de Habitaciones: Se deben poder gestionar el alta baja y modificación de Habitaciones de un tipo. Ejemplo.
+//            Si antes tenía 30 de tipo estándar, luego de agregar las habitaciones 201 al 205 de ese tipo, el conteo sumará 35 en total.
+    
     
 }
