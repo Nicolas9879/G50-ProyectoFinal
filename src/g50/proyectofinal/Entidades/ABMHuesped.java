be1fb1edf810ff4c0616bce8be5dc1755bac0b90
Nicolas <Nicolasgabrielsanchez7@gmail.com>
2023-10-03 11:43:34 -0300

    Update ABMHuesped.java

--- src/g50/proyectofinal/Entidades/ABMHuesped.java
+++ src/g50/proyectofinal/Entidades/ABMHuesped.java
@@ -29,6 +29,46 @@ public class ABMHuesped {
         this.correo = correo;
         this.celular = celular;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
     
     
     
