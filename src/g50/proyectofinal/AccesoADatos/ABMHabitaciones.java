eb7b91428bd9c9cb739d09242e9a3c772dcf81d1
Nicolas <Nicolasgabrielsanchez7@gmail.com>
2023-10-05 10:27:54 -0300

    Update ABMHabitaciones.java

--- src/g50/proyectofinal/AccesoADatos/ABMHabitaciones.java
+++ src/g50/proyectofinal/AccesoADatos/ABMHabitaciones.java
@@ -11,20 +11,16 @@ package g50.proyectofinal.AccesoADatos;
 public class ABMHabitaciones {
 //    ABM de Habitaciones: Se deben poder gestionar el alta baja y modificación de Habitaciones de un tipo. Ejemplo.
 //            Si antes tenía 30 de tipo estándar, luego de agregar las habitaciones 201 al 205 de ese tipo, el conteo sumará 35 en total.
-    
-    
+
     //-- Comentarios-- >> Seria un CRUD? un metodo para el Create (alta), otro para el Read, otro para el delete y uno al final para el update.
     // Deberia  de crearse en la base de datos.
-    
     // añadir habitaciones
-    
-    public void altaHabitacion(){
-         TipoDeHabitacion hab1=new TipoDeHabitacion();
+    public void altaHabitacion(TipoDeHabitacion habit) {
+        TipoDeHabitacion hab1 = new TipoDeHabitacion(habit.getPersonasmaximas(), habit.getCamas(),
+                habit.getTipocama(), habit.getPrecioxnoche(), habit.isEstado(), habit.getTipohabitacion());
+        
+         
          
     }
-    
-    
-    
-    
     
 }
