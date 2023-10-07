006a29e45586bf9c2c07b018e60353caae4db884
Nicolas <Nicolasgabrielsanchez7@gmail.com>
2023-10-07 17:56:41 -0300

    Update ABMReserva.java

--- src/g50/proyectofinal/AccesoADatos/ABMReserva.java
+++ src/g50/proyectofinal/AccesoADatos/ABMReserva.java
@@ -157,7 +157,7 @@ public class ABMReserva {
 
     public ABMReserva buscaReserva(ABMHuesped huesped) {
         ABMReserva reserva = new ABMReserva();
-1234567890123125125214125123123123
+123456789012312512521412
         String sql = "SELECT * FROM reserva WHERE dni=?";
         try {
             PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
