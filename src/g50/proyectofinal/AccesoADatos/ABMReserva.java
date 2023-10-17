d40ae1cf339c36750c9a2c8b7b388a349aeaaafb
Nicolas <Nicolasgabrielsanchez7@gmail.com>
2023-10-17 11:26:21 -0300

    Update ABMReserva.java

--- src/g50/proyectofinal/AccesoADatos/ABMReserva.java
+++ src/g50/proyectofinal/AccesoADatos/ABMReserva.java
@@ -139,7 +139,8 @@ public class ABMReserva {
         return habarray;
     }
 
-    public void crearReserva2(int dni, int numerohab) {
+    public void crearReserva2(int dni, int numerohab) {  //DEBE CAMBIAR EL ESTADO DE LA HABITACION...A  PARTIR DE SU NUMERO.. ADEMAS DE CREAR LA RESERVA Y NO SOLO DEVOLVER UN ARRAY.. SERIA LA 2DA PARTE DEL M
+        
 
     }
 
